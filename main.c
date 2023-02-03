121b97a55d6c785e76b33bee59c8648d6f1c4588
Quax2013 <robin.alex.ernst@gmail.com>
2023-02-03 10:27:33 +0100

    Start on logic

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
+#define EMPTY 0
+#define SOLID 1
+#define MOVING 2
+
+#define HEIGHT 18
+#define WIDTH 10
+
+int field[HEIGHT][WIDTH] = {};
+
+int main()
+{
     printf("Test");
 
     return 0;
