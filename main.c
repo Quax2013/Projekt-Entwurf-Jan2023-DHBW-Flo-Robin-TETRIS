31ed1ce823d82f266a64e87d3400d166c441fadd
fl012 <123734475+fl012@users.noreply.github.com>
2023-02-03 10:26:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<time.h>
+#include<stdlib.h>
 
-int main(){
-    printf("Test");
+
+void gotoxy(int x, int y);
+
+
+int main()
+{
+    system("cls");
+    gotoxy(1,1);    
+    int a = 8;
+    printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
 
     return 0;
+}
+
+
+// Quelle: https://www.quora.com/What-is-the-gotoxy-function-used-in-C Zugegriffen am 03.02.2023
+void gotoxy(int x, int y)
+{
+    HANDLE h=GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD position={x,y};
+    SetConsoleCursorPosition(h,position);
 }
