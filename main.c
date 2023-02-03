3735da782ddba05adcb719868b3b85a2386b3ff7
Quax2013 <robin.alex.ernst@gmail.com>
2023-02-03 11:00:18 +0100

    Array Output

--- main.c
+++ main.c
@@ -4,32 +4,43 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define EMPTY 0
-#define SOLID 1
-#define MOVING 2
+#define EMPTY 254
+#define MOVING 1
+#define SOLID 2
+
 
 #define HEIGHT 18
 #define WIDTH 10
 
 int field[HEIGHT][WIDTH] = {};
 
+
 void gotoxy(int x, int y);
 
 int main()
 {
-    system("cls");
-    gotoxy(1,1);    
-    int a = 8;
-    printf("Test");
+    char stop = "";
 
+        int i = 0;
+        system("cls");
+        //gotoxy(20 + i, 1);
+        int a = 8;
+        for (int i = 0; i < HEIGHT; i++){
+            for (int k = 0; k< WIDTH; k++){
+                printf("%d", field[i][k]);
+            }
+            printf("\n");
+        }
+
+
+    getche();
     return 0;
 }
-
 
 // Quelle: https://www.quora.com/What-is-the-gotoxy-function-used-in-C Zugegriffen am 03.02.2023
 void gotoxy(int x, int y)
 {
-    HANDLE h=GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD position={x,y};
-    SetConsoleCursorPosition(h,position);
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD position = {x, y};
+    SetConsoleCursorPosition(h, position);
 }
